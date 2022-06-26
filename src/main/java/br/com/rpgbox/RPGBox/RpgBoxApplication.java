5b5ef08f49ae85a84ed4073739ede32c7ab72c22
Joao Otavio <joaolima.felipelli@gmail.com>
2022-06-26 12:51:58 -0300

    Commit inicial

---
+++ src/main/java/br/com/rpgbox/RPGBox/RpgBoxApplication.java
@@ -0,0 +1,13 @@
+package br.com.rpgbox.RPGBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rpgbox/RPGBox/RpgBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rpgbox.RPGBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
